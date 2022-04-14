1d4ea3c279be4d074cdfab263c2141e49b2cdb99
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2022-04-14 14:07:32 -0700

    add isAlive stub and tests

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -4,5 +4,8 @@ class GameOfLife():
     def run(self):
         print("I don't do much, yet.")
 
+    def isAlive(self, map, x, y):
+      return False
+
 if __name__ == "__main__":
     GameOfLife().run()
--- test/test_game_of_life.py
+++ test/test_game_of_life.py
@@ -5,8 +5,24 @@ import contextlib
 from src.game_of_life import GameOfLife
 
 class GameOfLifeTest(unittest.TestCase):
+    # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    #  Any live cell with two or three live neighbours lives on to the next generation.
+    #  Any live cell with more than three live neighbours dies, as if by overpopulation.
+    #  Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-    def test_run_doesnt_do_much(self):
+    def test_isCellAlive(self):
+      game = GameOfLife()
+      map = [[0,1],[0,0]]
+
+      self.assertTrue(game.isAlive(map, 0, 1))
+
+    def test_isCellDead(self):
+      game = GameOfLife()
+      map = [[0,1],[0,0]]
+
+      self.assertTrue(game.isAlive(map, 0, 0))
+
+    def test_template(self):
         game = GameOfLife()
         fake_stdout = io.StringIO()
 
